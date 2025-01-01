b36dcffe40f2788133869a6497cddb248fb05e22
Jose Coves <jmbendito@gmail.com>
2025-01-01 13:23:58 -0800

    Add stats and filter option for problem tags
    User can select specific problem tags by id
    
    0. chinese remainder theorem: 1
    1. *special: 2
    2. 2-sat: 3
    3. ternary search: 3
    4. meet-in-the-middle: 4
    5. games: 8
    6. string suffix structures: 11
    7. matrices: 11
    8. interactive: 14
    9. graph matchings: 15
    10. fft: 15
    11. shortest paths: 16
    12. probabilities: 18
    13. hashing: 18
    14. flows: 22
    15. dsu: 29
    16. two pointers: 31
    17. geometry: 31
    18. divide and conquer: 36
    19. strings: 42
    20. bitmasks: 46
    21. number theory: 53
    22. combinatorics: 56
    23. sortings: 61
    24. dfs and similar: 63
    25. trees: 68
    26. binary search: 69
    27. graphs: 75
    28. brute force: 79
    29. constructive algorithms: 92
    30. implementation: 115
    31. math: 133
    32. data structures: 141
    33. greedy: 146
    34. dp: 168
    Enter tag ids (space or comma separated): (): 3

--- src/scripts/UnsolvedContestProblems/main.py
+++ src/scripts/UnsolvedContestProblems/main.py
@@ -6,6 +6,7 @@ from urllib.parse import quote_plus
 from rich.table import Table
 from rich.console import Console
 from rich.prompt import IntPrompt, Prompt, Confirm
+from collections import Counter
 
 sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../..")
 
@@ -54,6 +55,25 @@ def add_row(table, data, contest_mp):
     if url is None: url = ""
     table.add_row(cname, str(cid), pname, pid, str(rating), url)
 
+def getStats(problems):
+    c = Counter()
+    for problem in problems:
+        for tag in problem["tags"]:
+            c[tag] += 1
+    # for tag, count in c.most_common():
+    #     print(f"{tag}: {count}")
+    return c
+    
+def ask_numbers(prompt, default=None):
+    while True:
+        user_input = Prompt.ask(prompt, default=default or "")
+        try:
+            numbers = [int(x) for x in user_input.replace(",", " ").split()]
+            return numbers
+        except ValueError:
+            print("Invalid input. Please enter a list of numbers separated by spaces or commas.")
+
+
 def get_unsolved_problems_from_participated_contests(handle):
     submissions = get_submissions(handle)
     used_contest_ids = set()
@@ -85,6 +105,7 @@ def get_unsolved_problems_from_participated_contests(handle):
                     full_problem_name = str(cid + 1) + submission["problem"]["name"]
                     solved_problems.add(full_problem_name)
     all_problems = get_problems()
+    
     problems = []
     for problem in all_problems:
         is_problem_ok = False
@@ -98,7 +119,24 @@ def get_unsolved_problems_from_participated_contests(handle):
         if is_problem_ok:
             problems.append(problem)
     problems = problems[::-1] # reverse
-    return problems
+    counter = getStats(problems)
+    tags = []
+    for i, (tag, count) in enumerate(counter.most_common()[::-1]):
+        print(f"{i}. {tag}: {count}")
+        tags.append(tag)
+    tag_ids = ask_numbers("Enter tag ids (space or comma separated):")
+    if len(tag_ids) == 0: return problems
+    filtered_problems = []
+    for problem in problems:
+        is_problem_ok = False
+        for id in tag_ids:
+            assert(id >= 0 and id < len(tags))
+            if tags[id] in problem["tags"]:
+                is_problem_ok = True
+                break
+        if is_problem_ok:
+            filtered_problems.append(problem)
+    return filtered_problems
 
 def filter_by_rating(problems, rmin, rmax):
     def filter_problem(problem):
@@ -108,7 +146,7 @@ def filter_by_rating(problems, rmin, rmax):
     return filter(filter_problem, problems)
 
 def main():
-    handle = Prompt.ask("CodeForces handle")
+    handle = Prompt.ask("CodeForces handle", default='Jomax100')
 
     should_filter_by_rating = Confirm.ask(
         "Filter by rating",
